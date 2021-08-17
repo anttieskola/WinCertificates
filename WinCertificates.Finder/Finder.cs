dab92a883318dc88b7bc4e031d3d3d526668a4d2
Antti Eskola <antti.eskola@gmail.com>
2021-08-17 18:47:48 +0300

    find functionality and added self-contained folder publish profiles

---
+++ WinCertificates.Finder/Finder.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using WinCertificates.Models;
+
+namespace WinCertificates.Finder
+{
+    public class Finder
+    {
+        public static List<CertificateModel> Find(string thumbprint)
+        {
+            var certificates = new List<CertificateModel>();
+
+            var locationCount = Enum.GetValues(typeof(StoreLocation)).Length;
+            var nameCount = Enum.GetValues(typeof(StoreName)).Length;
+
+            var locationIndex = 0;
+            foreach (var location in Enum.GetValues(typeof(StoreLocation)))
+            {
+                locationIndex++;
+                var storeLocation = (StoreLocation)location;
+
+                var nameIndex = 0;
+                foreach (var name in Enum.GetValues(typeof(StoreName)))
+                {
+                    nameIndex++;
+
+                    var storeName = (StoreName)name;
+                    Console.WriteLine($"Location {locationIndex}/{locationCount} - Name {nameIndex}/{nameCount} ({storeLocation}.{storeName})");
+
+                    var store = new X509Store(storeName, storeLocation);
+                    store.Open(OpenFlags.ReadOnly);
+
+                    foreach (var cert in store.Certificates)
+                    {
+                        if (cert.Thumbprint == thumbprint)
+                        {
+                            certificates.Add(new CertificateModel
+                            {
+                                Id = Guid.NewGuid(),
+                                StoreLocation = storeLocation,
+                                StoreName = storeName,
+                                Thumbprint = cert.Thumbprint,
+                                SimpleName = cert.GetNameInfo(X509NameType.SimpleName, true),
+                                HasPublicKey = !string.IsNullOrEmpty(cert.GetPublicKeyString()),
+                                Verified = cert.Verify(),
+                            });
+                        }
+                    }
+
+                    store.Close();
+                }
+            }
+
+            return certificates;
+        }
+    }
+}
--- WinCertificates.Finder/Program.cs
+++ WinCertificates.Finder/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using WinCertificates.Models;
 
 namespace WinCertificates.Finder
 {
@@ -6,7 +7,30 @@ namespace WinCertificates.Finder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("TODO");
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Searches using given thumbprint all certificates found for current user");
+                Console.WriteLine("Usage: [thumbprint]");
+                return;
+            }
+
+            Console.WriteLine("Finding...");
+
+            var results = Finder.Find(args[0]);
+            Console.WriteLine("Results...");
+            
+            if (results.Count == 0)
+            {
+                Console.WriteLine("no certificate found with given thumbprint");
+            }
+            else
+            {
+                Console.WriteLine(CertificateModel.Headers());
+                foreach (var cert in results)
+                {
+                    Console.WriteLine(cert.ToString());
+                }
+            }
         }
     }
 }
